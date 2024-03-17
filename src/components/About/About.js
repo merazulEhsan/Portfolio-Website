eca9dd6a7bfb532c3fcfa7740c46505d099bef77
merazul2755 <merazulehsan880@gmail.com>
2024-03-18 01:51:37 +0600

    update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -48,7 +48,7 @@ const About = () => {
               </p>
               <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
                 <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                  <a href={pdf} target="_blank" rel="noopener noreferrer">
+                  <a href={pdf} rel="noopener noreferrer">
                     Download Resume
                   </a>
                 </span>
